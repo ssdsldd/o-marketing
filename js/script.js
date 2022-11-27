064c1e0f7939a3ffda8ee50885a8e8b6fba6d5ca
ssdsldd <noryaivanov@gmail.com>
2022-11-27 22:25:54 +0600

    adaptive header

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 $(function(){
-    let mixer = mixitup('.portfolio__gallery');
     $('.slider__content').slick({
         dots: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="/images/arrow-left.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="/images/arrow-right.svg" alt=""></button>',
     });
+    $('.header__btn').on('click', function(){
+        $('.header__btn').toggleClass("header__btn--active");
+        $('.header__list').toggleClass("header__list--active");
+        $('.header__logo').toggleClass("header__logo--active");
+    });
+    let mixer = mixitup('.portfolio__gallery');
 });

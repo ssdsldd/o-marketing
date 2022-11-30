342cd4e55a291b9d51d0f8ef215118defb65e5d7
ssdsldd <noryaivanov@gmail.com>
2022-11-30 22:56:10 +0600

    add adaptive

--- js/script.js
+++ js/script.js
@@ -3,6 +3,14 @@ $(function(){
         dots: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="/images/arrow-left.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="/images/arrow-right.svg" alt=""></button>',
+        responsive: [
+            {
+              breakpoint: 769,
+              settings: {
+                arrows: false
+              }
+            },
+          ]
     });
     $('.header__btn').on('click', function(){
         $('.header__btn').toggleClass("header__btn--active");
